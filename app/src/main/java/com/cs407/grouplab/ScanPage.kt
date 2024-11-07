e246ba8fb0db0df9205cb9ceae65702e32baa35a
ybao <bao39@wisc.edu>
2024-11-07 16:59:44 -0600

    QR Code Scanner completed(not test yet)

--- app/src/main/java/com/cs407/grouplab/ScanPage.kt
+++ app/src/main/java/com/cs407/grouplab/ScanPage.kt
@@ -12,6 +12,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.cs407.grouplab.databinding.ActivityMainBinding
 import com.cs407.grouplab.databinding.ActivityScanPageBinding
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
 
 class ScanPage : AppCompatActivity() {
     private val requestPermissionLauncher =
@@ -25,10 +28,36 @@ class ScanPage : AppCompatActivity() {
             }
         }
 
+
+    private val scanLauncher =
+        registerForActivityResult(ScanContract()){ result: ScanIntentResult ->
+            run{
+                if (result.contents == null){
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    setResult(result.contents)
+                }
+        }
+        }
+
     private lateinit var binding: ActivityScanPageBinding
 
-    private fun showCamera(){
 
+    private fun setResult(string: String){
+        binding.textResult.text = string
+    }
+
+    private fun showCamera(){
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        options.setPrompt("Scan QR Code")
+        options.setCameraId(0)
+        options.setBeepEnabled(false)
+        options.setBarcodeImageEnabled(true)
+        options.setOrientationLocked(false)
+
+        scanLauncher.launch(options)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,7 +79,7 @@ class ScanPage : AppCompatActivity() {
             showCamera()
         }
         else if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)){
-            Toast.makeText(contex, "CAMERA permission required", Toast.LENGTH_SHORT)
+            Toast.makeText(contex, "CAMERA permission required", Toast.LENGTH_SHORT).show()
         }
         else{
             requestPermissionLauncher.launch((android.Manifest.permission.CAMERA))
