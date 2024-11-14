add846bc31d7c344d87f2350ea0695a2f2bd38e6
YiMingBao0930 <bao39@wisc.edu>
2024-11-14 14:41:05 -0600

    Replace pie chart from NutritionPage to AppHomePage

--- app/src/main/java/com/cs407/grouplab/AppHomePage.kt
+++ app/src/main/java/com/cs407/grouplab/AppHomePage.kt
@@ -5,13 +5,23 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class AppHomePage : AppCompatActivity() {
+    private val Intake = floatArrayOf(98.8f, 123.8f, 161.6f,)
+    private val nutritionList = arrayOf("Protein", "Fat", "Carbohydrates")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_home_page)
+
+        setupPieChart()
 
 
 
@@ -44,6 +54,25 @@ class AppHomePage : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+
+    private fun setupPieChart() {
+        // Populating a list of PieEntries
+        val pieEntries = ArrayList<PieEntry>()
+        for (i in Intake.indices) {
+            pieEntries.add(PieEntry(Intake[i], nutritionList [i]))
+        }
+
+        // Creating the data set and setting colors
+        val dataSet = PieDataSet(pieEntries, "Your energy intake today")
+        dataSet.colors = ColorTemplate.COLORFUL_COLORS.toList()
+
+        // Creating PieData and setting it to the chart
+        val data = PieData(dataSet)
+        val chart = findViewById<PieChart>(R.id.chart)
+        chart.data = data
+        chart.animateY(2000)
+        chart.invalidate() // Refresh chart with new data
     }
 
 }
--- app/src/main/java/com/cs407/grouplab/NutritionPage.kt
+++ app/src/main/java/com/cs407/grouplab/NutritionPage.kt
@@ -13,16 +13,9 @@ import com.github.mikephil.charting.utils.ColorTemplate
 
 class NutritionPage : AppCompatActivity() {
 
-
-
-    private val Intake = floatArrayOf(98.8f, 123.8f, 161.6f,)
-    private val nutritionList = arrayOf("Protein", "Fat", "Carbohydrates")
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.nutrition_page)
-
-        setupPieChart()
 
         val signInButton: ImageButton = findViewById(R.id.jumptoscanpage)
         signInButton.setOnClickListener {
@@ -30,24 +23,5 @@ class NutritionPage : AppCompatActivity() {
             val intent = Intent(this, ScanPage::class.java)
             startActivity(intent)
         }
-    }
-
-    private fun setupPieChart() {
-        // Populating a list of PieEntries
-        val pieEntries = ArrayList<PieEntry>()
-        for (i in Intake.indices) {
-            pieEntries.add(PieEntry(Intake[i], nutritionList [i]))
-        }
-
-        // Creating the data set and setting colors
-        val dataSet = PieDataSet(pieEntries, "Your energy intake today")
-        dataSet.colors = ColorTemplate.COLORFUL_COLORS.toList()
-
-        // Creating PieData and setting it to the chart
-        val data = PieData(dataSet)
-        val chart = findViewById<PieChart>(R.id.chart)
-        chart.data = data
-        chart.animateY(2000)
-        chart.invalidate() // Refresh chart with new data
     }
 }
