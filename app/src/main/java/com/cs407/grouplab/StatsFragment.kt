3aaf8100055c8185c85b0ab8198ee38232527b24
Nathan Chanak <nchanak@wisc.edu>
2024-12-10 09:41:40 -0600

    Stats fragment now includes all the daily nutrition information. Uses scroll bar to fit everything. Looks nice too

--- app/src/main/java/com/cs407/grouplab/StatsFragment.kt
+++ app/src/main/java/com/cs407/grouplab/StatsFragment.kt
@@ -9,6 +9,8 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.cs407.grouplab.data.AppDatabase
@@ -33,20 +35,20 @@ class StatsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         db = AppDatabase.getDatabase(requireContext())
 
-        // Back button to navigate to AppHomePageFragment
-        val lastPageButton: ImageButton = view.findViewById(R.id.back_button)
-        lastPageButton.setOnClickListener {
-            val fragment = AppHomePageFragment()
-            parentFragmentManager.beginTransaction()
-                .setCustomAnimations(
-                    R.anim.slide_in_right,
-                    R.anim.slide_out_left,
-                    R.anim.slide_in_right,
-                    R.anim.slide_out_left
-                )
-                .replace(R.id.fragment_container, fragment)
-                .addToBackStack(null)
-                .commit()
+        // Set up the Toolbar
+        val toolbar: Toolbar = view.findViewById(R.id.exercise_toolbar)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
+
+        // Enable the back button in the toolbar
+        (requireActivity() as AppCompatActivity).supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+            title = "" // Hide the toolbar title text
+        }
+
+        // Handle back button click
+        toolbar.setNavigationOnClickListener {
+            parentFragmentManager.popBackStack() // Navigate back to the previous fragment
         }
 
         loadUserStats(view)
@@ -60,6 +62,23 @@ class StatsFragment : Fragment() {
         if (username != null) {
             lifecycleScope.launch(Dispatchers.IO) {
                 try {
+                    // Retrieve user goals
+                    val userGoal = db.userGoalDao().getUserGoal(username)
+                    val recommendedCalories = userGoal?.dailyCalories ?: 2000
+
+                    // Calculate recommended macros
+                    val recommendedCarbs = userGoal?.carbGram ?: (recommendedCalories * 0.5 / 4).toInt() // 50% from carbs
+                    val recommendedProtein = userGoal?.proteinGram ?: (recommendedCalories * 0.3 / 4).toInt() // 30% from protein
+                    val recommendedFat = userGoal?.fatGram ?: (recommendedCalories * 0.25 / 9).toInt() // 25% from fat
+                    val recommendedFiber = 25
+                    val recommendedSugar = 50
+                    val recommendedVitaminA = 900 // in mcg
+                    val recommendedVitaminB = 1.3 // in mg
+                    val recommendedVitaminC = 90 // in mg
+                    val recommendedVitaminD = 15 // in mcg
+
+
+                    // Retrieve daily nutrition log
                     val dailyNutrition = db.userNutritionLogDao().getDailyNutrition(username, currentDate)
 
                     withContext(Dispatchers.Main) {
@@ -67,73 +86,158 @@ class StatsFragment : Fragment() {
                         val caloriesSum = view.findViewById<TextView>(R.id.calories_sum)
                         caloriesSum.text = "${dailyNutrition.totalCalories}"
 
-                        // Distance progress bar (repurposed for total calories progress)
+                        // Calories Progress
                         val distanceProgressBar = view.findViewById<ProgressBar>(R.id.distance_progress_bar)
-                        val recommendedCalories = sharedPreferences.getInt("recommended_calories", 2000)
                         distanceProgressBar.max = recommendedCalories
-                        val caloriesProgress = dailyNutrition.totalCalories
-                        ObjectAnimator.ofInt(distanceProgressBar, "progress", caloriesProgress)
+                        ObjectAnimator.ofInt(distanceProgressBar, "progress", dailyNutrition.totalCalories)
                             .setDuration(2000)
                             .start()
                         val distanceText = view.findViewById<TextView>(R.id.distance_text)
-                        distanceText.text = "Calories: $caloriesProgress / $recommendedCalories"
+                        distanceText.text = "Calories: ${dailyNutrition.totalCalories} / $recommendedCalories"
 
-                        // Protein progress bar
+                        // Protein Progress
                         val proteinProgressBar = view.findViewById<ProgressBar>(R.id.protein_progress_bar)
-                        val recommendedProtein = (recommendedCalories * 0.3 / 4).toInt() // 30% of calories from protein
                         proteinProgressBar.max = recommendedProtein
-                        val proteinProgress = dailyNutrition.totalProtein
-                        ObjectAnimator.ofInt(proteinProgressBar, "progress", proteinProgress)
+                        ObjectAnimator.ofInt(proteinProgressBar, "progress", dailyNutrition.totalProtein)
                             .setDuration(2000)
                             .start()
                         val proteinText = view.findViewById<TextView>(R.id.protein_text)
-                        proteinText.text = "Protein: ${proteinProgress}g / ${recommendedProtein}g"
+                        proteinText.text = "Protein: ${dailyNutrition.totalProtein}g / ${recommendedProtein}g"
 
-                        // Fat progress bar
+                        // Fat Progress
                         val fatProgressBar = view.findViewById<ProgressBar>(R.id.fat_progress_bar)
-                        val recommendedFat = (recommendedCalories * 0.25 / 9).toInt() // 25% of calories from fat
                         fatProgressBar.max = recommendedFat
-                        val fatProgress = dailyNutrition.totalFat
-                        ObjectAnimator.ofInt(fatProgressBar, "progress", fatProgress)
+                        ObjectAnimator.ofInt(fatProgressBar, "progress", dailyNutrition.totalFat)
                             .setDuration(2000)
                             .start()
                         val fatText = view.findViewById<TextView>(R.id.fat_text)
-                        fatText.text = "Fat: ${fatProgress}g / ${recommendedFat}g"
+                        fatText.text = "Fat: ${dailyNutrition.totalFat}g / ${recommendedFat}g"
 
-                        // Update trans fat progress
+                        // Carbs Progress
+                        val carbsProgressBar = view.findViewById<ProgressBar>(R.id.carbs_progress_bar)
+                        carbsProgressBar.max = recommendedCarbs
+                        ObjectAnimator.ofInt(carbsProgressBar, "progress", dailyNutrition.totalCarbs)
+                            .setDuration(2000)
+                            .start()
+                        val carbsText = view.findViewById<TextView>(R.id.carbs_text)
+                        carbsText.text = "Carbs: ${dailyNutrition.totalCarbs}g / ${recommendedCarbs}g"
+
+                        // Update Fiber
+                        val fiberProgressBar = view.findViewById<ProgressBar>(R.id.fiber_progress_bar)
+                        fiberProgressBar.max = recommendedFiber
+                        ObjectAnimator.ofInt(fiberProgressBar, "progress", dailyNutrition.totalFiber)
+                            .setDuration(2000)
+                            .start()
+                        val fiberText = view.findViewById<TextView>(R.id.fiber_text)
+                        fiberText.text = "Fiber: ${dailyNutrition.totalFiber}g / ${recommendedFiber}g"
+
+                        // Update Sugar
+                        val sugarProgressBar = view.findViewById<ProgressBar>(R.id.sugar_progress_bar)
+                        sugarProgressBar.max = recommendedSugar
+                        ObjectAnimator.ofInt(sugarProgressBar, "progress", dailyNutrition.totalSugar)
+                            .setDuration(2000)
+                            .start()
+                        val sugarText = view.findViewById<TextView>(R.id.sugar_text)
+                        sugarText.text = "Sugar: ${dailyNutrition.totalSugar}g / ${recommendedSugar}g"
+
+                        // Trans Fat Progress
                         val transFatProgressBar = view.findViewById<ProgressBar>(R.id.trans_fat_progress_bar)
-                        transFatProgressBar.max = recommendedFat
+                        transFatProgressBar.max = (0.01 * recommendedCalories.toDouble()).toInt() // There is no recommended intake for trans fat
                         ObjectAnimator.ofInt(transFatProgressBar, "progress", dailyNutrition.totalTransFat)
                             .setDuration(2000)
                             .start()
+                        val transFatText = view.findViewById<TextView>(R.id.trans_fat_text)
+                        transFatText.text = "Trans Fat: ${dailyNutrition.totalTransFat}g / ${transFatProgressBar.max}g"
 
-                        // Update polyunsaturated fat progress
+                        // Polyunsaturated Fat Progress
                         val polyFatProgressBar = view.findViewById<ProgressBar>(R.id.polyunsaturated_fat_progress_bar)
-                        polyFatProgressBar.max = recommendedFat
+                        polyFatProgressBar.max = 25 // Example: 25g recommended
                         ObjectAnimator.ofInt(polyFatProgressBar, "progress", dailyNutrition.totalPolySaturatedFat)
                             .setDuration(2000)
                             .start()
+                        val polyFatText = view.findViewById<TextView>(R.id.polyunsaturated_fat_text)
+                        polyFatText.text = "Polyunsaturated Fat: ${dailyNutrition.totalPolySaturatedFat}g / 25g"
 
-                        // Update monounsaturated fat progress
+                        // Monounsaturated Fat Progress
                         val monoFatProgressBar = view.findViewById<ProgressBar>(R.id.monounsaturated_fat_progress_bar)
-                        monoFatProgressBar.max = recommendedFat
+                        monoFatProgressBar.max = 20 // Example: 20g recommended
                         ObjectAnimator.ofInt(monoFatProgressBar, "progress", dailyNutrition.totalMonoSaturatedFat)
                             .setDuration(2000)
                             .start()
+                        val monoFatText = view.findViewById<TextView>(R.id.monounsaturated_fat_text)
+                        monoFatText.text = "Monounsaturated Fat: ${dailyNutrition.totalMonoSaturatedFat}g / 20g"
 
-                        // Update sodium progress
+                        // Update Sodium
                         val sodiumProgressBar = view.findViewById<ProgressBar>(R.id.sodium_progress_bar)
-                        sodiumProgressBar.max = 2300 // Recommended daily sodium intake in mg
+                        sodiumProgressBar.max = 2300 // Recommended sodium intake in mg
                         ObjectAnimator.ofInt(sodiumProgressBar, "progress", dailyNutrition.totalSodium)
                             .setDuration(2000)
                             .start()
+                        val sodiumText = view.findViewById<TextView>(R.id.sodium_text)
+                        sodiumText.text = "Sodium: ${dailyNutrition.totalSodium}mg / 2300mg"
 
-                        // Update potassium progress
+                        // Update Potassium
                         val potassiumProgressBar = view.findViewById<ProgressBar>(R.id.potassium_progress_bar)
-                        potassiumProgressBar.max = 3500 // Recommended daily potassium intake in mg
+                        potassiumProgressBar.max = 3500 // Recommended potassium intake in mg
                         ObjectAnimator.ofInt(potassiumProgressBar, "progress", dailyNutrition.totalPotassium)
                             .setDuration(2000)
                             .start()
+                        val potassiumText = view.findViewById<TextView>(R.id.potassium_text)
+                        potassiumText.text = "Potassium: ${dailyNutrition.totalPotassium}mg / 3500mg"
+
+                        // Update Calcium
+                        val calciumProgressBar = view.findViewById<ProgressBar>(R.id.calcium_progress_bar)
+                        calciumProgressBar.max = 1000
+                        ObjectAnimator.ofInt(calciumProgressBar, "progress", dailyNutrition.totalCalcium)
+                            .setDuration(2000)
+                            .start()
+                        val calciumText = view.findViewById<TextView>(R.id.calcium_text)
+                        calciumText.text = "Calcium: ${dailyNutrition.totalCalcium}mg / ${1000}mg"
+
+                        // Update Iron
+                        val ironProgressBar = view.findViewById<ProgressBar>(R.id.iron_progress_bar)
+                        ironProgressBar.max = 18
+                        ObjectAnimator.ofInt(ironProgressBar, "progress", dailyNutrition.totalIron)
+                            .setDuration(2000)
+                            .start()
+                        val ironText = view.findViewById<TextView>(R.id.iron_text)
+                        ironText.text = "Iron: ${dailyNutrition.totalIron}mg / ${18}mg"
+
+                        // Update Vitamin A
+                        val vitaminAProgressBar = view.findViewById<ProgressBar>(R.id.vitamin_a_progress_bar)
+                        vitaminAProgressBar.max = recommendedVitaminA
+                        ObjectAnimator.ofInt(vitaminAProgressBar, "progress", dailyNutrition.totalVitaminA)
+                            .setDuration(2000)
+                            .start()
+                        val vitaminAText = view.findViewById<TextView>(R.id.vitamin_a_text)
+                        vitaminAText.text = "Vitamin A: ${dailyNutrition.totalVitaminA}mcg / ${recommendedVitaminA}mcg"
+
+                        // Update Vitamin B
+                        val vitaminBProgressBar = view.findViewById<ProgressBar>(R.id.vitamin_b_progress_bar)
+                        vitaminBProgressBar.max = (recommendedVitaminB * 100).toInt() // Convert mg to a more intuitive scale
+                        ObjectAnimator.ofInt(vitaminBProgressBar, "progress", (dailyNutrition.totalVitaminB * 100).toInt())
+                            .setDuration(2000)
+                            .start()
+                        val vitaminBText = view.findViewById<TextView>(R.id.vitamin_b_text)
+                        vitaminBText.text = "Vitamin B: ${dailyNutrition.totalVitaminB}mg / ${recommendedVitaminB}mg"
+
+                        // Update Vitamin C
+                        val vitaminCProgressBar = view.findViewById<ProgressBar>(R.id.vitamin_c_progress_bar)
+                        vitaminCProgressBar.max = recommendedVitaminC
+                        ObjectAnimator.ofInt(vitaminCProgressBar, "progress", dailyNutrition.totalVitaminC)
+                            .setDuration(2000)
+                            .start()
+                        val vitaminCText = view.findViewById<TextView>(R.id.vitamin_c_text)
+                        vitaminCText.text = "Vitamin C: ${dailyNutrition.totalVitaminC}mg / ${recommendedVitaminC}mg"
+
+                        // Update Vitamin D
+                        val vitaminDProgressBar = view.findViewById<ProgressBar>(R.id.vitamin_d_progress_bar)
+                        vitaminDProgressBar.max = recommendedVitaminD
+                        ObjectAnimator.ofInt(vitaminDProgressBar, "progress", dailyNutrition.totalVitaminD)
+                            .setDuration(2000)
+                            .start()
+                        val vitaminDText = view.findViewById<TextView>(R.id.vitamin_d_text)
+                        vitaminDText.text = "Vitamin D: ${dailyNutrition.totalVitaminD}mcg / ${recommendedVitaminD}mcg"
                     }
                 } catch (e: Exception) {
                     e.printStackTrace()
@@ -141,4 +245,6 @@ class StatsFragment : Fragment() {
             }
         }
     }
+
+
 }
